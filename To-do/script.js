98b013dfc5296c68d6ad04e178d704b78a201813
theicreator <saintkhizzy0@gmail.com>
2024-02-25 22:51:02 +0100

    changes

--- To-do/script.js
+++ To-do/script.js
@@ -9,7 +9,7 @@ const completedCount = document.querySelector(".numComplete");
 // Initial todo list
 let todos = [
   {
-    todo: "Buy more $WEN tokens for the bull run",
+    todo: "Buy more $WEN and $COM for the bull run",
     id: Date.now().toString(),
     completed: false,
   },
